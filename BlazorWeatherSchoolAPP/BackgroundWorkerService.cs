e043bf466b932b6b6c2c547fdbb6ad82f010c6f7
oskladal <ondrej.skladal@gmail.com>
2023-01-09 16:15:42 +0100

    CRON

---
+++ BlazorWeatherSchoolAPP/BackgroundWorkerService.cs
@@ -0,0 +1,35 @@
+﻿// Kde dochází k zapínání programu - spuštění - přidávání modulů 
+
+using System.Threading;
+using BlazorWeatherSchoolAPP;
+using WeatherAPI;
+using DBConnect;
+using WeatherAPI.Models;
+using Microsoft.AspNetCore.Components;
+
+public class BackgroundWorkerService : BackgroundService
+{
+    readonly ILogger<BackgroundWorkerService> _logger;
+
+    public BackgroundWorkerService(ILogger<BackgroundWorkerService> logger)
+    {
+        _logger = logger;
+    }
+
+    protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation("Sace mongoDB at:{time}", DateTimeOffset.Now);
+
+
+            
+            
+
+
+
+
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
